68ba78f6181e9f1aa20acd6ec5003e5327c47025
sebastian <sebastiangrundet@hotmail.dk>
2024-02-02 21:24:57 +0100

    try other craco.config.js variant

--- craco.config.js
+++ craco.config.js
@@ -1,16 +1,7 @@
 const million = require("million/compiler");
 
-
 module.exports = {
-
     webpack: {
-        configure: {
-            plugins: [
-                // MillionJs  (https://million.dev/docs/install)
-                million.webpack({
-                    auto: true,
-                })
-            ],
-        },
-    },
+        plugins: { add: [million.webpack({ auto: true })] }
+    }
 };
